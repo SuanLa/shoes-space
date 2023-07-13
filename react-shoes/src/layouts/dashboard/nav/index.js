b956ceb2d60e56d81fda700b5ed388bf5587ac79
XIpfengf <139424703+XIpfengf@users.noreply.github.com>
2023-07-13 14:43:54 +0800

    提交代码

--- react-shoes/src/layouts/dashboard/nav/index.js
+++ react-shoes/src/layouts/dashboard/nav/index.js
@@ -87,7 +87,7 @@ export default function Nav({ openNav, onCloseNav }) {
         </Link>
       </Box>
 
-      <NavSection data={login('admin')} />
+      <NavSection data={login('manager')} />
 
       <Box sx={{ flexGrow: 1 }} />
     </Scrollbar>
